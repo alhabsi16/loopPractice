a42e7e9a3c9883fb42942c05679670a13cf5e366
alhabsi16 <mxmx8283@gmail.com>
2023-06-04 11:53:07 +0400

    pr1

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace loop1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+
+            /*   
+               
+               *****
+               *   *
+               *   *
+               *   *
+               *****
+               
+            */
+             
+               
+            
+            for (int i = 0; i < 5; i++)
+            {
+
+                Console.Write("*" );
+                
+            }
+            Console.WriteLine();
+
+            for(int i = 0;i < 3;i++)
+            {
+                Console.WriteLine("*   *" );
+            }
+            for (int i = 0; i < 5; i++)
+            {
+
+                Console.Write("*");
+
+            }
+        }
+    }
+}
